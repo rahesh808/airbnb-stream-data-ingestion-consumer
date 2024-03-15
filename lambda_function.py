7617fd3c5853819258a31c24228099902eef2a10
rahesh <selvakumarrahesh@gmail.com>
2024-03-15 19:06:30 +0530

    Adding new lambda.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,32 +2,31 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    # Extract filtered booking records from the EventBridge event
-    filtered_bookings = event.get('filtered_bookings', [])
-    
-    # Write filtered booking records to S3 bucket
-    s3_bucket_name = 'airbnb-booking-records-rahesh'
-    s3_key = 'filtered_bookings.json'
-    s3_object = {'filtered_bookings': filtered_bookings}
-    
-    # Initialize S3 client
-    s3_client = boto3.client('s3')
-    
-    # Write data to S3 bucket
-    try:
-        response = s3_client.put_object(
+    # Check if the event is an SQS event
+        # Extract the body from the first record
+        body = json.loads(event[0]['body'])
+        
+        # Convert the body to a JSON string
+        body_json = json.dumps(body)
+        
+        booking_data = json.loads(body_json)
+        del booking_data['duration']
+        # Access the bookingId
+        booking_id = booking_data['bookingId']
+        print(booking_data)
+        # Store the JSON string in an S3 bucket
+        s3_bucket_name = 'airbnb-booking-records-rahesh'
+        s3_key = f'{booking_id}.json' 
+        s3_client = boto3.client('s3')
+        s3_client.put_object(
+            Body=body_json,
             Bucket=s3_bucket_name,
-            Key=s3_key,
-            Body=json.dumps(s3_object)
+            Key=s3_key
         )
-        print("Filtered bookings written to S3 successfully")
+        print("uploaded to s3")
+        
         return {
             'statusCode': 200,
-            'body': 'Filtered bookings written to S3 successfully'
+            'body': 'Successfully stored booking data in S3'
         }
-    except Exception as e:
-        print(f"Error writing filtered bookings to S3: {e}")
-        return {
-            'statusCode': 500,
-            'body': f'Error writing filtered bookings to S3: {e}'
-        }
+   
